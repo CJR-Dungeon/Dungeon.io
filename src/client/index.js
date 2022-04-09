f44cb4eb6e7eb908ccd8767cc3b31a12f3fa3680
Chattray <chattray.chea@gmail.com>
2022-04-08 20:36:07 -0700

    MVP code created

--- src/client/index.js
+++ src/client/index.js
@@ -2,20 +2,33 @@
 
 const { io } = require('socket.io-client');
 
+const { program } = require('commander');
+
 const SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:3000';
 
 const socket = io(SOCKET_URL);
 
 socket.emit('join', { clientId: 'testClient' });
 
-socket.emit('ready', { clientId: 'testClient' })
+program.parse();
+let choice = program.args[0] || 'start';
+console.log("I choose to", choice);
+if(choice === 'start' || !choice ) {
 
-socket.on('SEND-ENCOUNTER', (payload) => {
-  console.log(payload.flavorText)
-})
+  socket.emit('ready', { clientId: 'testClient' })
+  
+  socket.on('SEND-ENCOUNTER', (payload) => {
+    console.log(payload.flavorText)
+    process.exit();
+  })
+} else {
+  socket.emit('choice', { clientId: 'testClient', choice: choice })
+  
+  socket.on('resolution', (payload) => {
+    console.log(payload.result)
+    process.exit();
+  })
+}
 
-socket.emit('choice', { clientId: 'testClient', choice: 'counterattack'})
 
-socket.on('resolution', (payload) => {
-  console.log(payload.result)
-})
+
